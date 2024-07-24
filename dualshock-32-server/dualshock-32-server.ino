dd01c7e05b7120a85a6d2b1f0d24283f384ce792
DIALLO <ousmane.diallo1.sio@gmail.com>
2024-07-24 04:03:43 +0200

    feat: connect to controller & first commands

--- dualshock-32-server/dualshock-32-server.ino
+++ dualshock-32-server/dualshock-32-server.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Ps3Controller.h>
+#include <WiFi.h>
 
+void setup() {
+  Serial.begin(115200);
+  Ps3.attach(onCommandReceived);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("FC:B4:67:F6:1B:78"); // MAC address of ESP Server
+
+  Serial.println("Dualshock 32 Server is ready.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!Ps3.isConnected()) {
+    Serial.println("Waiting for controller connection...");
+    delay(100);
+    return; 
+  }
+  delay(2000);
+}
 
+void onCommandReceived() {
+  if (Ps3.event.button_down.cross) {
+    Serial.println("Cross pressed");
+  }
+  if (Ps3.event.button_up.cross) {
+    Serial.println("Cross released");
+  }
+
+  if (Ps3.event.button_down.triangle) {
+    Serial.println("Triangle presssed");
+  }
+
+  if (Ps3.event.button_down.square) {
+    Serial.println("Square pressed");
+  }
+
+  if (Ps3.event.button_down.circle) {
+    Serial.println("Circle pressed");
+  }
+}
+
+void onConnect() {
+  Serial.println("Connected.");
 }
